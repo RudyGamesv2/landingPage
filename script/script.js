54309f6fe542aa608d35aa140de674566fa40ab1
RudyGamesv2 <94765507+RudyGamesv2@users.noreply.github.com>
2022-07-14 21:27:12 +0200

    Update
    Update mobile hamburger menu, add Accessibility,  fix odder bug

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
 const mobileNav = document.querySelector('.nav-links');
 const burgerIcon = document.querySelector('.burger');
-const hideItem = document.querySelector(".home")
+
 
 
 burgerIcon.addEventListener('click', function(){
     mobileNav.classList.toggle('active');
     burgerIcon.classList.toggle('active');
-	hideItem.classList.toggle('hide')
 })
